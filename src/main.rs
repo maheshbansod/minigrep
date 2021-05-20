c1d969accc8c03e09de3e655d4497b719d609f6e
maheshbansod <coolkevin011@gmail.com>
2021-05-20 15:38:44 +0530

    error handling for application cod

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
@@ -13,7 +14,11 @@ fn main() {
   println!("Searching for {}", config.query);
   println!("In file {}", config.filename);
 
-  run(config);
+  if let Err(e) = run(config) {
+    println!("Application error: {}", e);
+    
+    process::exit(1);
+  }
 }
 
 struct Config {
@@ -34,9 +39,11 @@ impl Config {
   }
 }
 
-fn run(config: Config) {
-  let contents = fs::read_to_string(config.filename).expect("Something went wrong while reading the file");
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+  let contents = fs::read_to_string(config.filename)?;
 
   println!("Text:\n{}", contents);
+
+  Ok(())
 
 }

acabe1a23c99d687a87ac0f6021e2627fa36b567
Mahesh Bansod <coolkevin011@gmail.com>
2021-05-20 13:08:22 +0530

    read a file from commandline arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+  let args: Vec<String> = env::args().collect();
+
+  let query = &args[1];
+  let filename = &args[2];
+
+  println!("In file {}", filename);
+  let contents = fs::read_to_string(filename).expect("Something went wrong while reading the file");
+
+  println!("Text:\n{}", contents);
+
 }

82bd71723966b89a77119a16ab057c077a9220a1
maheshbansod <coolkevin011@gmail.com>
2021-05-21 18:03:49 +0530

    added tests for Cargo::new

--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,30 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
   Ok(())
 
 }
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  #[test]
+  pub fn args_len_check () {
+    let string1 = String::from("a");
+    let string2 = String::from("b");
+    let array = [string1, string2];
+    match Config::new(&array) {
+      Ok(_) => panic!("Not okay. Should give an arg error"),
+      _ => (),
+    };
+  }
+
+  #[test]
+  pub fn args_valid_check () -> Result<(), Box<dyn Error>>{
+    let arg1 = String::from("program");
+    let arg2 = String::from("query");
+    let arg3 = String::from("filename");
+    let args = [arg1, arg2, arg3];
+    let config = Config::new(&args)?;
+    assert_eq!(config.query, args[1]);
+    assert_eq!(config.filename, args[2]);
+    Ok(())
+  }
+}
